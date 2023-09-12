65bc867cafd4102f29ec3a0524e1b61b54b92bdf
ismet efe balik <ibalik@k1m32s10.42istanbul.com.tr>
2023-09-12 19:08:08 +0300

    executor

---
+++ main.c
@@ -0,0 +1,50 @@
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+void executor1(int fd1, int fd2, int *pipee)
+{
+
+    dup2(pipee[1], STDOUT_FILENO);
+    char * a[] = {"/bin/ls", "-la" , NULL};
+    execve(a[0], a, NULL);
+    close(pipee[1]);
+}
+
+void executor2(int fd1, int fd2, int *pipee)
+{
+
+
+    dup2(pipee[0], STDIN_FILENO);
+    char * a[] = {"/usr/bin/wc", "-c" , NULL};
+    execve(a[0], a, NULL);
+}
+
+int main(int argc, char const *argv[], char **env)
+{
+    int pipee[2];
+    pipe(pipee);
+    int pdchild1;
+    int pdchild2;
+    int status;
+    int fd1;
+    int fd2;
+
+    fd1 = open("girdi.c", O_WRONLY | O_CREAT | O_TRUNC, 0666);
+    fd2 = open("cikti.c", O_WRONLY | O_CREAT | O_TRUNC, 0666);
+    pdchild1 = fork();
+    if(pdchild1 == 0)
+        executor1(fd1 ,fd2, pipee);
+    waitpid (pdchild1, &status, WNOHANG | WUNTRACED);
+    if(pdchild1!=0)
+    {
+        pdchild2 = fork();
+        if(pdchild2 == 0)
+        {
+            executor2(fd1 ,fd2, pipee);
+            printf("******");
+        }
+    }
+    return 0;
+}
