508feb48d42faebddf2568324a0b5e514315fd30
Ali Akin <alakin@k1m30s11.42istanbul.com.tr>
2023-09-15 14:09:41 +0300

    uselesss

--- minishell.h
+++ minishell.h
@@ -29,6 +29,7 @@ typedef struct s_node
     char    **args;
     char    **outfile;
     char    **infile;
+<<<<<<< HEAD
     char    *path;
     int     pipe_count;
 }t_node;
@@ -41,5 +42,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 size_t	ft_strlen(const char *s);
 void    ft_readline(t_node *node);
 int     pipe_count(char *input, t_node *node);
+=======
+}t_node;
+>>>>>>> 194cc3201f20e9c56e175aee771412ef2ef9a06d
 
+int     env_count(char **envp);
+char    **env_list(char **envp, t_env *env);
+char	*ft_strdup(const char *s1);
+char	**ft_split(const char *s, char c);
 #endif
