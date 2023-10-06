98231fa4ada3bf84089ba29f65e6e3a0d11b38e9
ismet efe balik <ibalik@k1m31s07.42istanbul.com.tr>
2023-10-07 02:52:22 +0300

    norm start

--- exec/executor.c
+++ exec/executor.c
@@ -1,113 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   executor.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibalik <ibalik@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/07 02:18:30 by ibalik            #+#    #+#             */
+/*   Updated: 2023/10/07 02:43:21 by ibalik           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-static void first_process(t_data *data, int **pipes, int i)
+static void	first_process(t_data *data, int **pipes, int i)
 {
-    if (data->pipe_count > 0)
-    {
-        dup2(pipes[0][1], STDOUT_FILENO);
-    }
-    close_pipes(pipes, data->pipe_count);
-    op_router(data, i);
+	if (data->pipe_count > 0)
+	{
+		dup2(pipes[0][1], STDOUT_FILENO);
+	}
+	close_pipes(pipes, data->pipe_count);
+	op_router(data, i);
 }
 
-static int find_heredoc(char **operators)
+static int	find_heredoc(char **operators)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (operators[i] != NULL)
-    {
-        if (ft_strncmp(operators[i], "<<", 2) == 0)
-            return (1);
-    }
-    return (0);
+	i = 0;
+	while (operators[i] != NULL)
+	{
+		if (ft_strncmp(operators[i], "<<", 2) == 0)
+			return (1);
+	}
+	return (0);
 }
 
-static void middle_process(t_data *data, int **pipes, int i)
+static void	middle_process(t_data *data, int **pipes, int i)
 {
-    if (find_heredoc(data->nodes[i].operators) == 0)
-    {
-        dup2(pipes[i - 1][0], STDIN_FILENO);
-        close_pipes(pipes, data->pipe_count);
-    }    
-    close_pipes(pipes, data->pipe_count);
-    op_router(data, i);
+	if (find_heredoc(data->nodes[i].operators) == 0)
+	{
+		dup2(pipes[i - 1][0], STDIN_FILENO);
+		dup2(pipes[i][1], STDOUT_FILENO);
+	}
+	close_pipes(pipes, data->pipe_count);
+	op_router(data, i);
 }
 
-static void last_process(t_data *data, int **pipes, int i)
+static void	last_process(t_data *data, int **pipes, int i)
 {
-    if (find_heredoc(data->nodes[i].operators) == 0)
-    {
-        dup2(pipes[i - 1][0], STDIN_FILENO);
-    }  
-    close_pipes(pipes, data->pipe_count);
-    op_router(data, i);
+	if (find_heredoc(data->nodes[i].operators) == 0)
+	{
+		dup2(pipes[i - 1][0], STDIN_FILENO);
+	}
+	close_pipes(pipes, data->pipe_count);
+	op_router(data, i);
 }
 
-void update_status(int status, t_env **env)
+static void	wait_close_free(t_data *data, int **pipes, int *pids, t_env **env)
 {
-    char *tmpchar;
+	int		i;
+	char	*chart;
+	int		status;
 
-    tmpchar = ft_itoa(status);
-    update_env_node(*env, "?", tmpchar); //itoa leaks
-    free(tmpchar);   
+	chart = get_env_val("?", *env);
+	status = atoi(chart);
+	i = -1;
+	close_pipes(pipes, data->pipe_count);
+	while (++i < data->pipe_count + 1)
+	{
+		waitpid(pids[i], &status, 0);
+		update_status(status, env);
+	}
+	free(pids);
 }
 
-static void wait_close_free(t_data *data, int **pipes, int *pids, t_env **env)
+int	executor(t_data *data, t_env **env)
 {
-    int i;
-    char *chart;
-    int status;
+	int		**pipes;
+	pid_t	*pids;
+	int		i;
+	char	**envs;
 
-    chart = get_env_val("?", *env);
-    status = atoi(chart);
-    i = -1;
-    close_pipes(pipes, data->pipe_count);
-    while (++i < data->pipe_count + 1)
-    { 
-        waitpid(pids[i], &status, 0);
-        update_status(status, env);
-    }
-    free(pids);
-}
-
-int executor(t_data *data, t_env **env)
-{
-    int **pipes;
-    pid_t *pids;
-    int i;
-    char **envs;
-
-    find_env(data, env);
-
-    i = -1;
-    pipes = pipe_create(data->pipe_count);
-    pids = pid_create(data->pipe_count + 1);
-    while (++i < (data->pipe_count + 1))
-    {
-        if (data->nodes[i].is_builtin == 0 && 
-        data->nodes[i].is_valid_cmd == 1 &&
-        data->nodes[i].is_valid_path == 1
-        )
-        {
-            pids[i] = fork();
-            if (pids[i] == 0)
-            {
-                if (i == 0)
-                    first_process(data, pipes, i);
-                else if (i == data->pipe_count)
-                    last_process(data, pipes, i);
-                else
-                    middle_process(data, pipes, i);
-                if (data->nodes[i].is_valid_path == 1)
-                {
-                    envs = get_env_arr(*env); 
-                    execve(data->nodes[i].args[0], data->nodes[i].args, envs);
-                }
-                exit(0);
-            }
-        }
-    }
-    wait_close_free(data, pipes, pids, env);
-    return 0;
+	find_env(data, env);
+	i = -1;
+	pipes = pipe_create(data->pipe_count);
+	pids = pid_create(data->pipe_count + 1);
+	while (++i < (data->pipe_count + 1))
+	{
+		if (data->nodes[i].is_builtin == 0
+			&& data->nodes[i].is_valid_cmd == 1
+			&& data->nodes[i].is_valid_path == 1
+		)
+		{
+			pids[i] = fork();
+			if (pids[i] == 0)
+			{
+				if (i == 0)
+					first_process(data, pipes, i);
+				else if (i == data->pipe_count)
+					last_process(data, pipes, i);
+				else
+					middle_process(data, pipes, i);
+				if (data->nodes[i].is_valid_path == 1)
+				{
+					envs = get_env_arr(*env);
+					execve(data->nodes[i].args[0], data->nodes[i].args, envs);
+				}
+				exit(0);
+			}
+		}
+	}
+	wait_close_free(data, pipes, pids, env);
+	return (0);
 }
--- exec/op_router.c
+++ exec/op_router.c
@@ -1,67 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   op_router.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibalik <ibalik@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/07 02:12:05 by ibalik            #+#    #+#             */
+/*   Updated: 2023/10/07 02:46:38 by ibalik           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-static int outfiler(char **outfiles, char **operators, int *j)
+static	int	outfiler(char **outfiles, char **operators, int *j)
 {
-    int i;
-    int fd;
+	int	i;
+	int	fd;
 
-    i = -1;
-
-    while (outfiles[++i] && (((ft_strncmp(operators[i], ">", 1)) == 0) || ((ft_strncmp(operators[i], ">>", 2)) == 0)))
-    {
-        if ((ft_strncmp(operators[i], ">", 2)) == 0)
-        {
-            fd = open(outfiles[i], O_WRONLY | O_CREAT | O_TRUNC , 0777);
-        }
-        else if ((ft_strncmp(operators[i], ">>", 2)) == 0)
-        {
-            fd = open(outfiles[i], O_WRONLY | O_CREAT | O_APPEND, 0777);
-        }
-        (*j)++;
-    }
-    (*j)--;
-    return(fd);
+	i = -1;
+	while (outfiles[++i] && (((ft_strncmp(operators[i], ">", 1)) == 0)
+			|| ((ft_strncmp(operators[i], ">>", 2)) == 0)))
+	{
+		if ((ft_strncmp(operators[i], ">", 2)) == 0)
+			fd = open(outfiles[i], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+		else if ((ft_strncmp(operators[i], ">>", 2)) == 0)
+			fd = open(outfiles[i], O_WRONLY | O_CREAT | O_APPEND, 0777);
+		(*j)++;
+	}
+	(*j)--;
+	return (fd);
 }
 
-static void here_doc(t_data *data, int i)
+static void	here_doc(t_data *data, int i)
 {
-    char *tmp = data->nodes[i].infile[0];
-    char *buffer;
-    while (1)
-    {
-        buffer = readline(">");
-        if (ft_strncmp(buffer, tmp, sizeof(buffer)) == 0)
-        {
-            break;
-        }
+	char	*tmp;
+	char	*buffer;
 
-    }
+	tmp = data->nodes[i].infile[0];
+	while (1)
+	{
+		buffer = readline(">");
+		if (ft_strncmp(buffer, tmp, sizeof(buffer)) == 0)
+			break ;
+		free(buffer);
+	}
 }
 
-void op_router(t_data *data, int i)
+void	op_router(t_data *data, int i)
 {
-    int j;
-    char *pwd;
+	int		j;
+	char	*pwd;
 
-    j = -1;
-    while (data->nodes[i].operators[++j] != NULL)
-    {
-        if (ft_strncmp(data->nodes[i].operators[j], ">", 2) == 0)
-            dup2(outfiler(data->nodes[i].outfile, data->nodes[i].operators, &j), STDOUT_FILENO);
-        else if (ft_strncmp(data->nodes[i].operators[j], ">>", 2) == 0)
-            dup2(outfiler(data->nodes[i].outfile, data->nodes[i].operators, &j), STDOUT_FILENO);
-        else if (ft_strncmp(data->nodes[i].operators[j], "<", 2) == 0)
-        {
-            pwd = return_pwd();
-            pwd = ft_strjoin(pwd, "/");
-            pwd = ft_strjoin(pwd, data->nodes[i].infile[0]);
-            if ((access(pwd, F_OK)) == 0)
-                dup2(open(data->nodes[i].infile[0], O_RDONLY, 0777), STDIN_FILENO);
-            else
-                data->nodes[i].args[1] = ft_strdup(data->nodes[i].infile[0]);
-            free(pwd);
-        }
-        else if (ft_strncmp(data->nodes[i].operators[j], "<<", 2) == 0)
-            here_doc(data, i);
-    }
+	j = -1;
+	while (data->nodes[i].operators[++j] != NULL)
+	{
+		if (ft_strncmp(data->nodes[i].operators[j], ">", 2) == 0)
+			dup2(outfiler(data->nodes[i].outfile,
+					data->nodes[i].operators, &j), STDOUT_FILENO);
+		else if (ft_strncmp(data->nodes[i].operators[j], ">>", 2) == 0)
+			dup2(outfiler(data->nodes[i].outfile,
+					data->nodes[i].operators, &j), STDOUT_FILENO);
+		else if (ft_strncmp(data->nodes[i].operators[j], "<", 2) == 0)
+		{
+			pwd = return_pwd();
+			pwd = ft_strjoin(pwd, "/");
+			pwd = ft_strjoin(pwd, data->nodes[i].infile[0]);
+			if ((access(pwd, F_OK)) == 0)
+				dup2(open(data->nodes[i].infile[0], O_RDONLY, 0777),
+					STDIN_FILENO);
+			else
+				data->nodes[i].args[1] = ft_strdup(data->nodes[i].infile[0]);
+			free(pwd);
+		}
+		else if (ft_strncmp(data->nodes[i].operators[j], "<<", 2) == 0)
+			here_doc(data, i);
+	}
 }
