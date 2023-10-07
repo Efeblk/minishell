e722741097b76348c42a79e868081646c8b9dee9
ismet efe balik <ibalik@k1m31s07.42istanbul.com.tr>
2023-10-07 04:27:13 +0300

    more norm

--- builtin/builtin_start.c
+++ builtin/builtin_start.c
@@ -6,40 +6,46 @@
 /*   By: ibalik <ibalik@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 02:26:27 by ibalik            #+#    #+#             */
-/*   Updated: 2023/10/07 04:14:58 by ibalik           ###   ########.fr       */
+/*   Updated: 2023/10/07 04:26:10 by ibalik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void is_builtin(char *cmd, t_data *data, int i, t_env **env, t_export **exp_list)
+static void	helper_builtin(t_data *data, t_env **env, t_export **exp, int i)
+{
+	if (ft_strncmp(data->nodes[i].cmd, "clear", 5) == 0)
+		printf("\033[2J\033[H");
+	else if (ft_strncmp(data->nodes[i].cmd, "exit", 4) == 0)
+		run_exit(data, env, i);
+	else if (ft_strncmp(data->nodes[i].cmd, "pwd", 3) == 0)
+		run_pwd();
+	else if (ft_strncmp(data->nodes[i].cmd, "cd", 2) == 0)
+		run_cd(data, i);
+	else if (ft_strncmp(data->nodes[i].cmd, "echo", 4) == 0)
+		run_echo(data, i);
+	else if (ft_strncmp(data->nodes[i].cmd, "env", 3) == 0)
+		run_env(*env);
+	else if (ft_strncmp(data->nodes[i].cmd, "export", 6) == 0)
+		run_export(exp, i, data, env);
+	else if (ft_strncmp(data->nodes[i].cmd, "unset", 5) == 0)
+		run_unset(env, exp, i, data);
+	else
+		data->nodes[i].is_builtin = 0;
+}
+
+static void	is_builtin(t_data *data, int i, t_env **env, t_export **exp_list)
 {
 	int	flag;
 
 	data->nodes[i].is_builtin = 1;
 	flag = question_mark(data, env);
-	if (cmd != NULL)
+	if (data->nodes[i].cmd != NULL)
 	{
-		if (flag == 1)
-			;
-		else if (ft_strncmp(cmd, "clear", 5) == 0)
-			printf("\033[2J\033[H");
-		else if (ft_strncmp(cmd, "exit", 4) == 0)
-			run_exit(data, env, i);
-		else if (ft_strncmp(cmd, "pwd", 3) == 0)
-			run_pwd();
-		else if (ft_strncmp(cmd, "cd", 2) == 0)
-			run_cd(data, i);
-		else if (ft_strncmp(cmd, "echo", 4) == 0)
-			run_echo(data, i);
-		else if (ft_strncmp(cmd, "env", 3) == 0)
-			run_env(*env);
-		else if (ft_strncmp(cmd, "export", 6) == 0)
-			run_export(exp_list, i, data, env);
-		else if (ft_strncmp(cmd, "unset", 5) == 0)
-			run_unset(env, exp_list, i, data);
-		else
-			data->nodes[i].is_builtin = 0;
+		if (flag == 0)
+		{
+			helper_builtin(data, env, exp_list, i);
+		}
 	}
 	else
 		data->nodes[i].is_builtin = 0;
@@ -52,7 +58,7 @@ void	built_in(t_data *data, t_env **env, t_export **exp_list)
 	i = -1;
 	while (++i < data->pipe_count + 1)
 	{
-		is_builtin(data->nodes[i].cmd, data, i, env, exp_list);
+		is_builtin(data, i, env, exp_list);
 		if (data->nodes[i].is_builtin == 1)
 			update_status(0, env);
 	}
